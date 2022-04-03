7023a6d6512e2bc0192ae89987aa2ccfd12711ff
Zach <zach@zachsims.com>
2022-04-03 16:32:59 -0400

    Updated image

--- supporter.user.js
+++ supporter.user.js
@@ -149,7 +149,7 @@ async function run() {
                 resolve({template_ctx: template_ctx, template_img: img})
             }
             img.onerror = reject
-            img.src = "https://rplacesuperstonk.github.io/rplace-image/reference.png?tstamp=" + Math.floor(Date.now() / 10000);
+            img.src = "https://Simsz.github.io/reference.png?tstamp=" + Math.floor(Date.now() / 10000);
         })
     }
 
