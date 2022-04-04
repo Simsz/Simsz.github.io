fc7e2d3516d4977bd731a03949a01b828c0233da
Zach <zach@zachsims.com>
2022-04-04 14:58:08 -0400

    Updated user script

--- supporter.user.js
+++ supporter.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Tarik rplace overlay
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  support overlay + clicking if enabled
-// @author       kotri
+// @author       Kotri, forked from halfdanes script
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
@@ -36,19 +36,34 @@ async function run() {
     }
 
     const colors = {
+        0:  "#6D001A",
         2:  "#FF4500",
         3:  "#FFA800",
         4:  "#FFD635",
+        5:  "#FFF8B8",
         6:  "#00A368",
+        7:  "#00CC78",
         8:  "#7EED56",
+        9:  "#00756F",
+        10: "#009EAA",
+        11: "#00CCC0",
         12: "#2450A4",
         13: "#3690EA",
         14: "#51E9F4",
+        15: "#493AC1",
+        16: "#6A5CFF",
+        17: "#94B3FF",
         18: "#811E9F",
         19: "#B44AC0",
+        20: "#E4ABFF",
+        21: "#DE107F",
+        22: "#FF3881",
         23: "#FF99AA",
+        24: "#6D482F",
         25: "#9C6926",
+        26: "#FFB470",
         27: "#000000",
+        28: "#515252",
         29: "#898D90",
         30: "#D4D7D9",
         31: "#FFFFFF",
@@ -57,70 +72,71 @@ async function run() {
         colors[v] = k;
     }
 
-    function checkbox(id, labelText, checked, onclick) {
-        const elementDiv = document.createElement("div");
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = checked;
-        checkbox.id = id;
-        checkbox.onclick = onclick;
-
-        const label = document.createElement("label");
-        label.htmlFor = id;
-        label.appendChild(document.createTextNode(labelText));
-
-        elementDiv.appendChild(checkbox);
-        elementDiv.appendChild(label);
-        return elementDiv;
+    function mlEmbed(){
+        return document.querySelector("mona-lisa-embed");
     }
 
-    function coordinates(minmax, value1, onchange1, value2, onchange2){
-        const elementDiv = document.createElement("div");
-
-        let checkbox;
-        checkbox = document.createElement("input");
-        checkbox.type = "number";
-        checkbox.value = value1;
-        checkbox.placeholder = "X "+minmax;
-        checkbox.addEventListener('change', onchange1);
-        checkbox.style.cssText = "width: 100px;"
-        elementDiv.appendChild(checkbox);
-
-        checkbox = document.createElement("input");
-        checkbox.type = "number";
-        checkbox.value = value2;
-        checkbox.placeholder = "Y "+minmax;
-        checkbox.addEventListener('change', onchange2);
-        checkbox.style.cssText = "width: 100px;"
-        elementDiv.appendChild(checkbox);
-
-        return elementDiv;
+    function info(string) {
+        mlEmbed().querySelector("#timeout_text").textContent = string;
     }
 
     function generateForm(){
-        const ml = document.querySelector("mona-lisa-embed");
+        const ml = mlEmbed();
 
-        const form = document.createElement("form");
+        var template = Handlebars.compile(`
+<div style="position: absolute; top: 40%; left: 0; background-color: green; padding: 20px; border: thick double rgb(50, 161, 206); border-radius: 10px;">
+    <h1>Tarik's r/place supporter</h1>
+    <p>Forked from halfdane</p>
+    <form id="control-panel">
+        <div><h1><input type="checkbox" id="should_run"><label for="should_run">Activate automatic clicking</label></h1></div>
+        <h2>Restrict clicking area to these coordinates:</h2>
+        <div>
+            <input type="number" placeholder="X Min" style="width: 100px;" id="x_min">
+            <input type="number" placeholder="Y Min" style="width: 100px;" id="y_min">
+            <button type="button" id="use_current_min" style="background-color: lightgray;">Use currently selected coordinate</button>
+        </div>
+        <div>
+            <input type="number" placeholder="X Max" style="width: 100px;" id="x_max">
+            <input type="number" placeholder="Y Max" style="width: 100px;" id="y_max">
+            <button type="button" id="use_current_max" style="background-color: lightgray;">Use currently selected coordinate</button>
+        </div>
+        <div><p id="timeout_text"></p></div>
+        <div><input type="checkbox" id="should_debug"><label for="should_debug">Debug (don't actually place any tiles)</label></div>
+        <div><input type="checkbox" id="should_show_overlay" checked><label for="should_show_overlay">Show the Overlay</label></div>
+    </form>
+</div>
+`);
+        ml.innerHTML += template();
 
-        form.appendChild(checkbox("should_run", "Run", run, function (event){run = event.target.checked;} ));
-        form.appendChild(checkbox("should_debug", "Debug", debug, function (event){debug = event.target.checked;} ));
-        form.appendChild(checkbox("should_show_overlay", "Overlay", true, function (event){
+        const form = ml.querySelector("#control-panel");
+
+        form.querySelector('#should_run').onclick = function (event){run = event.target.checked;};
+        form.querySelector('#should_debug').onclick = function (event){debug = event.target.checked;};
+        form.querySelector('#should_show_overlay').onclick = function (event){
             const parent = ml.shadowRoot.querySelector("mona-lisa-canvas").shadowRoot.querySelector("div")
             const template_canvas = parent.querySelector("#template-canvas");
             template_canvas.style.display = (event.target.checked ? "block" : "none")
-        } ));
+        };
 
-        form.appendChild(coordinates("Min",
-            x_min, function (event){x_min = event.target.value;}, y_min, function (event){y_min = event.target.value;}))
-        form.appendChild(coordinates("Max",
-            x_max, function (event){x_max = event.target.value;}, y_max, function (event){y_max = event.target.value;}))
+        form.querySelector('#use_current_min').onclick = function (event){
+            const pos = mlEmbed().shadowRoot.querySelector("mona-lisa-coordinates").shadowRoot.querySelector("div").textContent;
+            var arr = /\((.*),(.*)\)/.exec(pos);
+            x_min=arr[1];
+            y_min=arr[2];
+        };
 
-        const div = document.createElement("div");
-        div.appendChild(form);
-        div.style.cssText = "position: absolute;top:"+X_OFFSET+"px;left: 700px;background-color: green; padding:20px;";
-        console.log(div)
-        ml.appendChild(div);
+        form.querySelector('#use_current_max').onclick = function (event){
+            const pos = mlEmbed().shadowRoot.querySelector("mona-lisa-coordinates").shadowRoot.querySelector("div").textContent;
+            var arr = /\((.*),(.*)\)/.exec(pos);
+            x_max=arr[1];
+            y_max=arr[2];
+        };
+
+        form.querySelector('#x_min').addEventListener('change', function (event){x_min = event.target.value;});
+        form.querySelector('#y_min').addEventListener('change', function (event){y_min = event.target.value;});
+        form.querySelector('#x_max').addEventListener('change', function (event){x_max = event.target.value;});
+        form.querySelector('#y_max').addEventListener('change', function (event){y_max = event.target.value;});
     }
 
     function createOrGetTemplateCanvas(parent){
@@ -131,7 +147,7 @@ async function run() {
         const template_canvas = document.createElement("canvas");
         template_canvas.id = 'template-canvas';
         parent.appendChild(template_canvas);
-        template_canvas.style.cssText = "position: absolute;top: "+Y_OFFSET+"px; left: "+X_OFFSET+"px;opacity: 100%;"
+        template_canvas.style.cssText = "position: absolute;top: "+Y_OFFSET+"px; left: "+X_OFFSET+"px;opacity: 50%;"
         return template_canvas;
     }
 
@@ -141,16 +157,15 @@ async function run() {
             img.crossOrigin = "Anonymous";
             img.onload = () => {
                 const template_canvas = createOrGetTemplateCanvas(ml_canvas.parentElement);
-                template_canvas.width = 128;
-                template_canvas.height = 176;
+                template_canvas.width = img.width;
+                template_canvas.height = img.height;
                 const template_ctx = template_canvas.getContext("2d");
-                template_ctx.drawImage(img, 0, 0, 64, 88);
+                template_ctx.drawImage(img, 0, 0);
 
                 resolve({template_ctx: template_ctx, template_img: img})
             }
             img.onerror = reject
-            img.src = "https://Simsz.github.io/overlay.png?tstamp=" + Math.floor(Date.now() / 10000);
-            img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 64px;height: 88px;opacity: 100%";
+            img.src = "https://Simsz.github.io/reference.png?tstamp=" + Math.floor(Date.now() / 10000);
         })
     }
 
@@ -186,46 +201,44 @@ async function run() {
             const {template_ctx, template_img} = await get_template_ctx(canvas);
 
 
-            let x1 = (X_OFFSET<=x_min && x_min<=template_img.width+X_OFFSET) ? x_min : X_OFFSET;
-            let x2 = (x1<x_max && x_max<template_img.width+X_OFFSET) ? x_max : template_img.width+X_OFFSET;
-            let y1 = (Y_OFFSET<y_min && y_min<template_img.height+Y_OFFSET) ? y_min : Y_OFFSET;
-            let y2 = (y1<y_max && y_max<template_img.height+Y_OFFSET) ? y_max : template_img.height+Y_OFFSET;
+            x_min = (X_OFFSET<=x_min && x_min<=template_img.width+X_OFFSET) ? x_min : X_OFFSET;
+            x_max = (x_min<=x_max && x_max<=template_img.width+X_OFFSET) ? x_max : template_img.width+X_OFFSET;
+            y_min = (Y_OFFSET<=y_min && y_min<=template_img.height+Y_OFFSET) ? y_min : Y_OFFSET;
+            y_max = (y_min<=y_max && y_max<=template_img.height+Y_OFFSET) ? y_max : template_img.height+Y_OFFSET;
 
-            console.log("focus area is", x1, x2, y1, y2);
+            mlEmbed().querySelector("#x_min").value = x_min;
+            mlEmbed().querySelector("#y_min").value = y_min;
+            mlEmbed().querySelector("#x_max").value = x_max;
+            mlEmbed().querySelector("#y_max").value = y_max;
 
-            if (!run) {
-                await sleep(1_000);
-                continue;
-            }
-            const ctx = canvas.getContext('2d');
-            const errors = []
+            if (run) {
+                const ctx = canvas.getContext('2d');
+                const errors = []
 
+                for (let x = x_min; x <= x_max; x++) {
+                    for (let y = y_min; y <= y_max; y++) {
+                        let correct = getPixel(template_ctx, x - X_OFFSET, y-Y_OFFSET);
+                        let actual = getPixel(ctx, x, y);
+                        if (actual !== correct) {
+                            errors.push({x: x, y: y, correct: correct, actual: actual});
+                        }
+                    }
+                }
 
+                if (errors.length > 0) {
+                    var e = errors[Math.floor(Math.random()*errors.length)];
 
-            for (let x = x1; x < x2; x++) {
-                for (let y = y1; y < y2; y++) {
-                    let correct = getPixel(template_ctx, x - X_OFFSET, y-Y_OFFSET);
-                    let actual = getPixel(ctx, x, y);
-                    if (actual !== correct) {
-                        errors.push({x: x, y: y, correct: correct, actual: actual});
+                    console.log("(%s / %s) is %c%s%c but should be %c%s", e.x, e.y,
+                        "background:"+e.actual, e.actual, "background:inherit;",
+                        "background:"+e.correct, e.correct
+                    )
+
+                    await setPixel(canvas, e.x, e.y, e.correct);
+                    if (!debug){
+                        edited = true;
                     }
                 }
             }
-
-            if (errors.length > 0) {
-                var e = errors[Math.floor(Math.random()*errors.length)];
-
-                console.log("(%s / %s) is %c%s%c but should be %c%s", e.x, e.y,
-                    "background:"+e.actual, e.actual, "background:inherit;",
-                    "background:"+e.correct, e.correct
-                )
-
-                await setPixel(canvas, e.x, e.y, e.correct);
-                if (!debug){
-                    edited = true;
-                }
-            }
-
         } catch (error){
             console.log("ignoring", error);
         } finally {
@@ -236,9 +249,9 @@ async function run() {
                 timeout =Math.floor(Math.random() * 5_000);
             }
             if (debug){
-                timeout = 100;
+                timeout = 1_000;
             }
-            console.log("sleeping for ", timeout);
+            info("sleeping for " + timeout + "ms");
             await sleep(timeout);
         }
     }
